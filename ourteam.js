94df21c29a54f49d033b15beb8dc7029e7be74de
AndreaChiari <chiariandrea94@gmail.com>
2022-12-21 16:38:11 +0100

    update-js

--- ourteam.js
+++ ourteam.js
@@ -1,12 +1,7 @@
 console.log('JS OK')
 
-//creo delle immagini e le definisco con una variabile
-let container = document.createElement('div');
-console.log(container)
-let img = document.createElement("img");
-img.src = "wayne-barnett-founder-ceo.jpg";
-container.appendChild(img);
-
+//prendo gli elementi dal DOM
+let main = document.getElementById('main');
 
 //creo degli oggetti con le informazioni richieste
 const teamList = [
@@ -16,14 +11,16 @@ const teamList = [
 {name: 'Angela Lopez', role: 'Social Media Manager', img: 'angela-lopez-social-media-manager.jpg'},
 {name: 'Scott Estrada', role: 'Developer', img: 'scott-estrada-developer.jpg'},
 {name: 'Barbara Ramos', role: 'Graphic designer', img: 'barbara-ramos-graphic-designer.jpg'}
-    
+
 ]
 console.log(teamList)
 
 //creo un ciclo for nel quale vado ad inserire ogni singolo elemento e stamparlo in pagina
-let textName = document.createElement("div");
-for(i=0; i < teamList['names'].length; i++ ){
-    let totalNames = teamList['names'][i];
-    textName.innerText = totalNames;
-    console.log(totalNames);
+
+for(i=0; i < teamList.length; i++ ){
+    let totalNames = teamList[i];
+    let container = document.createElement('div');
+    main.append(container)
+    container.append(totalNames);
+    console.log(container);
 }
