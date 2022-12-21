3e803d325486e728109f5b54ff098233600569c4
AndreaChiari <chiariandrea94@gmail.com>
2022-12-21 15:41:16 +0100

    update

--- ourteam.js
+++ ourteam.js
@@ -2,14 +2,13 @@ console.log('JS OK')
 
 //creo delle immagini e le definisco con una variabile
 let container = document.createElement('div');
+console.log(container)
 let img = document.createElement("img");
 img.src = "wayne-barnett-founder-ceo.jpg";
 container.appendChild(img);
 
-r
 
 //creo un array con le informazioni richieste
-
 const teamList = {
     names: ['wayne barnett','Walter Gordon','Scott Estrada','Barbara Ramos','Angela Lopez','Barbara Ramos'],
     roles: ['Founder and CEO','Chief Editor','Office Manager','Social Media Manager','Developer','Graphic Designer']
@@ -19,7 +18,6 @@ const teamList = {
 
 
 //creo un ciclo for nel quale vado ad inserire ogni singolo elemento e stamparlo in pagina
-
 let textName = document.createElement("div");
 for(i=0; i < teamList['names'].length; i++ ){
     let totalNames = teamList['names'][i];
