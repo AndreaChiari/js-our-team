b3bae056f1273f557afadccf1f01bfce54dbf94f
AndreaChiari <chiariandrea94@gmail.com>
2022-12-21 15:20:16 +0100

    update

--- ourteam.js
+++ ourteam.js
@@ -10,7 +10,7 @@ Container.appendChild(img);
 
 //creo un array con le informazioni richieste
 
-let teamList = {
+const teamList = {
     names: ['wayne barnett','Walter Gordon','Scott Estrada','Barbara Ramos','Angela Lopez','Barbara Ramos'],
     roles: ['Founder and CEO','Chief Editor','Office Manager','Social Media Manager','Developer','Graphic Designer']
     
@@ -21,8 +21,9 @@ console.log(teamList['names'][0])
 //creo un ciclo for nel quale vado ad inserire ogni singolo elemento e stamparlo in pagina
 
 for(i=0; i < teamList['names'].length; i++ ){
-    let textName = document.createElement('p')
-    const names = teamList['names'][i];
-    textName.appendChild('names');
+    const textName = document.createElement("p");
+    const totalNames = teamList['names'][i];
+    textName.innerText(totalNames);
+    console.log(totalNames);
     
 }
